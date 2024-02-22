f0ba5aed551fd0df720e5291912f7992ca47bdf0
Sergii Diachenko <sdiachen@softserveinc.com>
2024-02-22 19:38:57 +0200

    rectangles done

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,6 +1,24 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, another: &Rectangle) -> bool {
+        self.width >= another.width && self.height >= another.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -8,10 +26,19 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    println!("Area of the rectangle is {}", area(rect1));
-}
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 
-fn area(rect: Rectangle) -> u32 {
-    rect.width * rect.height
+    let sq1=Rectangle::square(12);
+
+    println!("Square of {} size has area: {}", sq1.width, sq1.area());
 }
