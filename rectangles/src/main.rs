cb2216f4e6899983753b5c417d6b55f7a973dca8
Sergii Diachenko <sdiachen@softserveinc.com>
2024-02-22 18:35:16 +0200

    rect with struct

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,9 +1,17 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!("Area of the rectangle is {}", area(rect1));
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rect: Rectangle) -> u32 {
+    rect.width * rect.height
 }
