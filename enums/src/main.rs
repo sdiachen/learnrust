6229b11d5815e0c34b6625740a2e228439b03c46
Sergii Diachenko <sdiachen@softserveinc.com>
2024-02-26 22:14:07 +0200

    try match

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,10 +1,22 @@
-enum Coin{
+#[derive(Debug)]
+enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter
+    Quarter,
+}
+
+fn value_in_cents(coin: &Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let coin = Coin::Nickel;
+
+    println!("Value of coin {:?} is {}", coin, value_in_cents(&coin));
 }
