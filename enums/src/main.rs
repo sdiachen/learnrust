395aa585ab886c7f8febdf2cda7180ef99bd2699
Sergii Diachenko <sdiachen@softserveinc.com>
2024-02-26 22:34:48 +0200

    enums done

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -3,7 +3,13 @@ enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska
 }
 
 fn value_in_cents(coin: &Coin) -> u8 {
@@ -11,12 +17,15 @@ fn value_in_cents(coin: &Coin) -> u8 {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
 
 fn main() {
-    let coin = Coin::Nickel;
+    let coin = Coin::Quarter(UsState::Alaska);
 
     println!("Value of coin {:?} is {}", coin, value_in_cents(&coin));
 }
